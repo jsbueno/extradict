8848b4dfaab9377d463a3974dbdc1b980d8531cc
Joao S. O. Bueno <gwidion@gmail.com>
2016-05-27 02:08:43 -0300

    Ditto - version bumped to 0.1.2

--- setup.py
+++ setup.py
@@ -2,23 +2,11 @@
 
 from setuptools import setup
 
-def get_str_in_line(line):
-    quotechar = "'" if "'" in line else "\""
-    return line.split(quotechar)[-2]
-
-with open("extradict/__init__.py") as f:
-    for line in f:
-        if "__author__" in line:
-            __author__ = get_str_in_line(line)
-        elif "__version__" in line:
-            __version__ = get_str_in_line(line)
-
-
 setup(
     name = 'extradict',
-    version = __version__,
+    version = "0.1.2",
     license = "LGPLv3+",
-    author = __author__,
+    author = "João S. O. Bueno",
     author_email = "gwidion@gmail.com",
     description = "Enhanced, maybe useful, data containers. For now, a versioned dict and an ordered versioned dict",
     keywords = "versioned transactional container collection dict dictionary",
