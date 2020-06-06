acbd994714c7b971801326d006eb8e1dfb37df69
Joao S O Bueno <gwidion@gmail.com>
2020-06-06 02:39:20 -0300

    Implements balanced delete, item retrieval

--- extradict/binary_tree_dict.py
+++ extradict/binary_tree_dict.py
@@ -6,18 +6,44 @@ from collections.abc import MutableMapping
 
 _empty = object()
 
+
+class EmptyNode:
+    __slots__ = ()
+    depth = 0
+
+    def __bool__(self):
+        return False
+
+    def __len__(self):
+        return 0
+
+    def __repr__(self):
+        return "EmptyNode"
+
+    def __str__(self):
+        return ""
+
+    def get(self):
+        raise KeyError(key)
+
+    def delete(self, key):
+        raise KeyError(key)
+
+EmptyNode = EmptyNode()  # The KISS Singleton
+
+
 class PlainNode:
     __slots__ = "key value left right key_func".split()
-    def __init__(self, key, value=_empty, left=None, right=None, key_func=None):
+    def __init__(self, key, value=_empty, left=EmptyNode, right=EmptyNode, key_func=None):
         self.key = key
         self.value = value if value != _empty else key
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
         self.key_func = None
 
     @property
     def leaf(self):
-        return self.left is None and self.right is None
+        return not (self.left or self.right)
 
     def _cmp_key(self, key):
         return self.key_func(key) if self.key_func else key
@@ -26,7 +52,7 @@ class PlainNode:
         self_key = self._cmp_key(self.key)
         new_key = self._cmp_key(key)
         if new_key == self_key and replace:
-            self.value = value if value is not empty else key
+            self.value = value if value is not _empty else key
             return
         bin_name = "left" if new_key < self_key else "right"
         bin = getattr(self, bin_name)
@@ -35,16 +61,45 @@ class PlainNode:
         else:
             setattr(self, bin_name, type(self)(key=key, value=value, key_func=self.key_func))
 
+    def get(self, key):
+        self_key = self._cmp_key(self.key)
+        new_key = self._cmp_key(key)
+        if self_key == new_key:
+            return self
+        elif new_key > self_key:
+            return self.right.get(key)
+        return self.left.get(key)
+
+
+    @property
+    def depth(self):
+        return max(self.left.depth, self.right.depth) + 1
+
+    def _mute_into(self, other):
+        self.key = other.key
+        self.value = other.value
+        self.key_func = other.key_func
+
     def delete(self, key):
-        raise NotImplementedError()
+        self_key = self._cmp_key(self.key)
+        new_key = self._cmp_key(key)
+
+        if self_key == new_key:
+            if self.leaf:
+                return EmptyNode
+            target_str = "left" if self.left.depth > self.right.depth else "right"
+            target = getattr(self, target_str)
+            self._mute_into(target)
+            key = target.key  # key to delete in target subtree.
+        else:
+            target_str = "left" if new_key < self_key else "right"
+            target = getattr(self, target_str)
+
+        setattr(self, target_str, target.delete(key))
+        return self
 
     def __len__(self):
-        if self.leaf:
-            return 1
-        return 1 + (len(self.left) if self.left else 0) + (len(self.right) if self.right else 0)
-
-
-
+        return 1 + len(self.left) + len(self.right)
 
 
 class AVLNode(PlainNode):
@@ -64,7 +119,7 @@ class TreeDict(MutableMapping):
     def __getitem__(self, key):
         if not self.root:
             raise KeyError(key)
-        self.root.getitem(key)
+        self.root.get(key).value
 
     def __setitem__(self, key, value):
         if self.root is None:
@@ -78,7 +133,7 @@ class TreeDict(MutableMapping):
         self.root.delete(key)
 
     def __iter__(self):
-        return iter(self.root) if self.root else iter(())
+        return (n.value for n in self.root) if self.root else iter(())
 
     def __len__(self):
         return len(self.root) if self.root else 0
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -5,8 +5,8 @@ import pytest
 def test_node_value_equals_key_if_not_given():
     n = PlainNode(0)
     assert n.value == 0
-    assert n.left is None
-    assert n.right is None
+    assert not n.left
+    assert not n.right
     assert n.key_func is None
     assert n.leaf
 
@@ -15,7 +15,7 @@ def test_node_insert_right():
     n = PlainNode(0)
     n.insert(10)
     assert n.right.value == 10
-    assert n.left is None
+    assert not n.left
     assert not n.leaf
 
 
@@ -23,7 +23,7 @@ def test_node_insert_left():
     n = PlainNode(0)
     n.insert(-10)
     assert n.left.value == -10
-    assert n.right is None
+    assert not n.right
     assert not n.leaf
 
 
@@ -40,6 +40,7 @@ def test_node_insert_right_left():
     n.insert(5)
     assert n.right.left.value == 5
 
+
 def test_node_insert_both_sides():
     n = PlainNode(0)
     n.insert(10)
@@ -48,6 +49,22 @@ def test_node_insert_both_sides():
     assert n.right.value == 10
     assert n.left.value == -10
     assert n.right.left.value == 5
+
+
+@pytest.mark.parametrize(
+    ("values", "expected"), [
+            ((0,), 1),
+            ((0,5), 2),
+            ((0, 5, -5), 2),
+            ((0, 5, -5, 7), 3),
+            ((0, 5, -5, 7, 10), 4), # not auto balancing
+            ((0, 5, -5, 7, 10, -7), 4),
+])
+def test_depth(values, expected):
+    root = PlainNode(values[0])
+    for v in values[1:]:
+        root.insert(v)
+    assert root.depth == expected
 
 
 def test_node_len():
@@ -59,3 +76,156 @@ def test_node_len():
     assert len(n) == 3
     n.insert(-10)
     assert len(n) == 4
+
+
+def test_node_delete_leaf():
+    n = PlainNode(0)
+    n.insert(5)
+    n.delete(5)
+    assert not n.right
+    assert not n.left
+
+
+def test_node_delete_root_single_node():
+    n = PlainNode(0)
+    assert not(n.delete(0))
+
+
+def test_node_delete_root_one_branch():
+    n = PlainNode(0)
+    n.insert(5)
+    n.delete(0)
+    assert n.value == 5
+    assert n.depth == 1
+    assert not n.left and not n.right
+
+
+def test_node_delete_root_one_branch_to_left():
+    n = PlainNode(0)
+    n.insert(-5)
+    n.delete(0)
+    assert n.value == -5
+    assert n.depth == 1
+    assert not n.left and not n.right
+
+
+def test_node_delete_root_two_branches():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(-5)
+    n.delete(0)
+    assert n.value in (5, -5)
+    assert bool(n.left) ^ bool(n.right)
+
+
+def test_node_delete_branch():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(10)
+    n.delete(5)
+    assert n.value is 0
+    assert n.right.value == 10
+    assert n.right.depth == 1
+    assert not n.right.left and not n.right.right
+
+
+def test_node_delete_branch_left():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(3)
+    n.delete(5)
+    assert n.value is 0
+    assert n.right.value == 3
+    assert n.right.depth == 1
+    assert not n.right.left and not n.right.right
+
+
+def test_node_delete_branch_2_leaves():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(10)
+    n.insert (3)
+    n.delete(5)
+    assert n.value is 0
+    assert n.right.value in (10, 3)
+    assert n.right.depth == 2
+    assert n.right.left and n.right.left.value == 3 or n.right.right and n.right.right.value == 10
+    assert bool(n.right.left) ^ bool(n.right.right)
+
+
+def test_node_delete_non_existing_value():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(10)
+    n.insert (3)
+    n.insert(-5)
+    with pytest.raises(KeyError):
+        n.delete(7)
+
+
+def test_node_get_root_identity():
+    n = PlainNode(0)
+    assert n.get(0) is n
+
+
+def test_node_get_root_value():
+    n = PlainNode(0, "zero")
+    assert n.get(0).value == "zero"
+
+
+def test_node_get_leave_identity():
+    n = PlainNode(0)
+    n.insert(5)
+    assert n.get(5) is n.right
+
+
+def test_node_get_leave_value():
+    n = PlainNode(0, "zero")
+    n.insert(5, "five")
+    assert n.get(5).value == "five"
+
+
+def test_node_get_branch_identity():
+    n = PlainNode(0)
+    n.insert(5)
+    n.insert(10)
+    assert n.get(5) is n.right
+
+
+def test_node_get_branch_value():
+    n = PlainNode(0, "zero")
+    n.insert(5, "five")
+    n.insert(10, "ten")
+    assert n.get(5).value == "five"
+
+
+def test_node_get_identity_or_value_independs_other_nodes():
+    n = PlainNode(0, "zero")
+    n.insert(5)
+    n.insert(10, "ten")
+    assert n.get(5).value == 5
+
+
+def test_node_insert_replaces_value():
+    n = PlainNode(0, "zero")
+    n.insert(5, "five")
+    n.insert(10, "ten")
+    assert n.get(5) is n.right
+    assert n.get(5).value == "five"
+    assert len(n) == 3
+    n.insert(5, "fake")
+    assert n.get(5) is n.right
+    assert n.get(5).value == "fake"
+    assert len(n) == 3
+
+
+def test_node_insert_can_create_new():
+    n = PlainNode(0, "zero")
+    n.insert(5, "five")
+    n.insert(10, "ten")
+    assert n.get(5).value == "five"
+    assert len(n) == 3
+    n.insert(5, "fake", replace=False)
+    assert n.get(5).value == "five"  # Retrieves first inserted value
+    assert len(n) == 4
+    assert n.get(5).right.left.value == "fake"
