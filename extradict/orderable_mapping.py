2cda1c1abc48abfa7c3d5dcc1c219684aa6493bc
Joao S O Bueno <gwidion@gmail.com>
2022-06-17 03:30:01 -0300

    OrderableMapping with custom keys!

--- extradict/orderable_mapping.py
+++ extradict/orderable_mapping.py
@@ -18,16 +18,150 @@
 
 # (type annotations temporarily provided for doc. puroposes only)
 
-from collections.abc import Mapping, MutableMapping
+from collections.abc import Mapping, MutableMapping, MutableSequence
 from copy import copy
 from threading import RLock
+
+class _LLNode:
+    __slots__ = "value next prev".split()
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+    def __repr__(self):
+        return f"_N<{'^' if self.prev is None else '...'}, {self.value!r}, {'$' if self.next is None else '...'}>"
+
+
+class _LL(MutableSequence):
+    # Naive linked list to go along OrderableMapping
+    def __init__(self, initial=None):
+        self.head = None
+        self.tail = None
+        self.lock = RLock()
+        self._length = 0
+        if initial:
+            for item in initial:
+                self.append(item)
+
+            #initial = iter(initial)
+            #self.head = _LLNode(next(initial, None))
+            #prev = self.head
+            #for value in initial:
+                #prev.next = current = _LLNode(value)
+                #current.prev = prev
+        #self.tail = current
+
+    def __getitem__(self, index):
+        return self._getnode(index).value
+
+    def _getnode(self, index):
+        if not self.head:
+            raise IndexError()
+        current = self.head if index >= 0 else self.tail
+        for i in range(0, index, 1 if index > 0 else -1):
+            with self.lock:
+                if index > 0:
+                    with self.lock:
+                        current = current.next
+                else:
+                    current = current.prev
+            if current is None:
+                raise IndexError()
+        return current
+
+    def __setitem__(self, index, value):
+        if index >= len(self):
+            raise IndexError()
+        node_at_index = _getnode(index)
+        node_at_index.value = value
+        prev = node_at_index.prev
+
+    def _insert_before_node(self, node, value):
+        if not isinstance(value, _LLNode):
+            value = _LLNode(value)
+        else:
+            value.next = value.prev = None
+        with self.lock:
+            if node.prev:
+                value.prev = node.prev
+                node.prev.next = value
+            else:
+                if node is not self.head:
+                    raise ValueError("Reference node for insertion not part of this linkedlist")
+                self.head = value
+            value.next = node
+            node.prev = value
+
+    def insert(self, index, value):
+        node = _LLNode(value)
+        if index >= len(self):
+            self.append(value)
+            return
+        node_at_index = self._getnode(index)
+        prev = node_at_index.prev
+        with self.lock:
+            if prev:
+                prev.next = node
+            else:
+                self.head = node
+            node.next = node_at_index
+            node_at_index.prev = node
+            self._length += 1
+
+    def append(self, value):
+        node = _LLNode(value)
+        with self.lock:
+            if self.tail is None:
+                self.head = self.tail = node
+            else:
+                node.prev = self.tail
+                node.prev.next = node
+                self.tail = node
+            self._length += 1
+
+    def _iter_nodes(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+    def __iter__(self):
+        for node in self._iter_nodes():
+            yield node.value
+
+    def __delitem__(self, index):
+        node_at_index = self._getnode(index)
+        with self.lock:
+            if node_at_index.prev:
+                node_at_index.prev.next = node_at_index.next
+            if node_at_index.next:
+                node_at_index.next.prev = node_at_index.prev
+            self._length -= 1
+
+    def __len__(self):
+        return self._length
+
+    def __copy__(self):
+        # Nodes have to be recreated on a shallow copy:
+        return self.__class__(iter(self))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({list(self)})"
+
 
 class OrderableMapping(MutableMapping):
     """Mapping allowing custom ordenation of inserted keys.
 
     By default, insertion order is used. A custom key maybe passed,
-    as a callable similar to the one passed to "sorted' builtin.
+    as a callable similar to the one passed to "sorted' builtin, but which takes
+    the key and value of each dictionary item as positional parameters.
 
+    Example: instance that will keep the keys ordered by the value order:
+    mymap = OrderableMapping(key=lambda k, v: v)
+
+    Insertions and retrieval by position (.get_at) are O(N) for custom keys, O(1) for
+    insertion-order key. (Iterating keys in sorted order is linear in both cases)
 
     """
     def __init__(
@@ -37,8 +171,9 @@ class OrderableMapping(MutableMapping):
             key = None,
         ):
         self.data = {}
-        self.order = []
+        self.order = [] if key is None else _LL()
         self.lock = RLock()
+        self._key = key
         self._inserting = False  # state used when inserting a field at a specific position
         if not initial:
             return
@@ -47,13 +182,26 @@ class OrderableMapping(MutableMapping):
         for key,value in initial:
             self[key] = value
 
+    key = property(lambda self: self._key)
+
     def __getitem__(self, key):
         return self.data[key]
 
     def __setitem__(self, key, value):
         with self.lock:
-            if key not in self.data and not self._inserting:
-                self.order.append(key)
+            if self.key:
+                insertion_key = self.key(key, value)
+                for index, order_node in enumerate(self.order._iter_nodes()):
+                    order_key = order_node.value
+                    order_value = self.data[order_key]
+                    if insertion_key < self.key(order_key, order_value):
+                        self.order._insert_before_node(order_node, key)
+                        break
+                else:
+                    self.order.append(key)
+            else:
+                if key not in self.data and not self._inserting:
+                        self.order.append(key)
             self.data[key] = value
 
     def __delitem__(self, key):
@@ -61,7 +209,14 @@ class OrderableMapping(MutableMapping):
             del self.data[key]
             self.order.remove(key)
 
+    def get_at(self, index):
+        """retrieve the item (key, value) at index position. i.e.: use the ordering instead of the key"""
+        key = self.order[index]
+        return key, self.data[key]
+
     def insert(self, pos, key, value):
+        if self.key:
+            raise TypeError("Can't insert item at arbitrary position for custom-keyed mappings")
         with self.lock:
             self._inserting = True
             self[key] = value
@@ -69,6 +224,8 @@ class OrderableMapping(MutableMapping):
             self._inserting = False
 
     def move(self, key, pos):
+        if self.key:
+            raise TypeError("Can't move item for custom-keyed mappings")
         with self.lock:
             self.order.remove(key)
             self.order.insert(pos, key)
@@ -77,7 +234,7 @@ class OrderableMapping(MutableMapping):
         return copy(self)
 
     def __copy__(self):
-        instance = self.__class__()
+        instance = self.__class__(key=self.key)
         with instance.lock, self.lock:
             instance.data = copy(self.data)
             instance.order = copy(self.order)
--- tests/test_orderable_mapping.py
+++ tests/test_orderable_mapping.py
@@ -1,4 +1,7 @@
+from copy import copy
+
 from extradict import OrderableMapping
+from extradict.orderable_mapping import _LL
 
 import pytest
 
@@ -33,6 +36,12 @@ def test_orderable_mapping_insert_at_pos():
     a.insert(1, "d", 4)
     assert list(a.keys()) == ["a", "d", "b", "c"]
 
+def test_orderable_mapping_get_at():
+    # low level test: if implementation changes,feel free to drop this test
+    a = OrderableMapping({"a": 1, "b": 2, "c": 3})
+    a.insert(1, "d", 4)
+    assert a.get_at(2) == ("b", 2)
+
 
 def test_orderable_mapping_insert_at_pos_keeps_order_on_failure():
     # low level test: if implementation changes,feel free to drop this test
@@ -57,4 +66,53 @@ def test_orderable_mapping_copy():
     a["c"] = 3
     with pytest.raises(KeyError):
         b["c"]
+
+def test_orderable_mapping_custom_key_for_keys():
+    a = OrderableMapping({0: 1}, key=lambda k, v: k)
+    a[20] = 2
+    a[10] = 3
+    assert list(a.keys()) == [0, 10, 20]
+
+def test_orderable_mapping_custom_key_for_values():
+    a = OrderableMapping({0: 10, 1: 30, 3: 20}, key=lambda k, v: v)
+    a[4] = -10
+    assert list(a.values()) == [-10, 10, 20, 30]
+
+
+def test_orderable_mapping_custom_key_cant_be_set_after_instantiation():
+    a = OrderableMapping()
+    with pytest.raises(AttributeError):
+        a.key = lambda k, v: v
+
+
+# Embedded linked list
+@pytest.fixture
+def linkedlist():
+    return _LL(range(5))
+
+def test_linkedlist_is_iterable(linkedlist):
+    assert list(linkedlist) == list(range(5))
+
+def test_linkedlist_insert(linkedlist):
+    linkedlist.insert(1, 10)
+    assert list(linkedlist) == [0, 10, 1, 2, 3, 4]
+
+def test_linkedlist_insert_at_0(linkedlist):
+    linkedlist.insert(0, 10)
+    assert list(linkedlist) == [10, 0, 1, 2, 3, 4]
+
+def test_linkedlist_append(linkedlist):
+    linkedlist.append(5)
+    assert list(linkedlist) == [0, 1, 2, 3, 4, 5,]
+
+def test_linkedlist_del(linkedlist):
+    del linkedlist[1]
+    assert list(linkedlist) == [0, 2, 3, 4,]
+
+def test_linkedlist_copiable(linkedlist):
+    b = copy(linkedlist)
+    b.insert(0, -1)
+    assert list(linkedlist) == list(range(5))
+    assert list(b) == [-1, 0, 1, 2, 3, 4,]
+
 
