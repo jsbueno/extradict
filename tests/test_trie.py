a52abee430301bd26a69e089e11edbf6127e54e8
Joao S O Bueno <gwidion@gmail.com>
2022-09-03 00:29:32 -0300

    Implements pattern trie

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -1,32 +1,56 @@
-from extradict.trie import CharTrie
+import pytest
+
+from extradict.trie import PrefixCharTrie, PatternCharTrie
+from extradict.trie import _WORD_END, _ENTRY_END
 
 
 def test_chartrie_can_store_text():
-    a = CharTrie()
+    a = PrefixCharTrie()
     a.add("test")
     assert "test" in a
 
 
 def test_chartrie_simple_prefix_works():
-    a = CharTrie(initial=["car", "carpet"])
+    a = PrefixCharTrie(initial=["car", "carpet"])
     assert a["car"].contents == {"car", "carpet"}
 
-def test_chartrie_simple_prefix_works_and_exclude_others():
-    a = CharTrie(initial=["car", "carpet", "java", "javascript"])
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_chartrie_len_works(cls):
+    a = cls(initial=["car", "carpet"])
+    assert len(a) == 2
+
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_chartrie_iter_works(cls):
+    a = cls(initial=["car", "carpet"])
+    assert sorted(a) == ["car", "carpet"]
+
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_chartrie_works_and_exclude_others(cls):
+    a = cls(initial=["car", "carpet", "java", "javascript"])
     assert a["car"].contents == {"car", "carpet"}
 
-def test_chartrie_update_works():
-    a = CharTrie(initial=["car", "carpet"])
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_chartrie_update_works(cls):
+    a = cls(initial=["car", "carpet"])
     a.update(["java", "javascript"])
     assert a["jav"].contents == {"java", "javascript"}
 
-def test_chartrie_len_works():
-    a = CharTrie(initial=["car", "carpet", "banana"])
-    assert len(a) == 3
 
-def test_modifying_prefixed_tree_updates_original():
-    from copy import copy
-    a = CharTrie(initial=["car"])
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_chartrie_raises_on_non_existing_pattern(cls):
+    a = cls(initial=["car", "carpet"])
+    with pytest.raises(KeyError):
+        a["java"]
+
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+@pytest.mark.parametrize("sentinel", [_WORD_END,  _ENTRY_END])
+def test_trie_keys_cant_contain_sentinel_values(cls, sentinel):
+    a = cls(initial=["car", "carpet"])
+    with pytest.raises(ValueError):
+        a.add("java" + sentinel)
+
+def test_modifying_prefixed_trie_updates_original():
+    a = PrefixCharTrie(initial=["car"])
     assert len(a) == 1
     a["car"].add("pet")
     assert len(a) == 2
@@ -34,15 +58,39 @@ def test_modifying_prefixed_tree_updates_original():
     assert "pet" not in a
     assert "carpet" in a["car"]
 
+def test_modifying_prefixed_pattern_trie_raises_value_error():
+    a = PatternCharTrie(initial=["car"])
+    with pytest.raises(ValueError):
+        a["car"].add("pet")
 
-def test_shallow_copy_creates_indepent_chartrie():
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_shallow_copy_creates_indepent_chartrie(cls):
     from copy import copy
-    a = CharTrie(initial=["car"])
+    a = cls(initial=["car"])
     b = copy(a)
     b.add("carpet")
     assert len(b) == 2
     assert len(a) == 1
     assert "carpet" not in a
 
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_copy_method_creates_indepent_chartrie(cls):
+    a = cls(initial=["car"])
+    b = a.copy()
+    b.add("carpet")
+    assert len(b) == 2
+    assert len(a) == 1
+    assert "carpet" not in a
+
+@pytest.mark.skip
+@pytest.mark.parametrize("cls", (PrefixCharTrie, PatternCharTrie))
+def test_discard_method(cls):
+    a = cls(initial=["car", "carpet"])
+    a.discard("car")
+    assert len(a) == 1
+    assert "car" not in a and "carpet" in a
 
 
+def test_pattern_chartrie_simple_prefix_works():
+    a = PatternCharTrie(initial=["car", "carpet", "oscar"])
+    assert a["car"].contents == {"car", "carpet", "oscar"}
