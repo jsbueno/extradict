b1ac1c1ac070d94fb7f4abdeed32d768d53b453c
João S. O. Bueno <gwidion@gmail.com>
2023-10-03 13:05:46 -0300

    Bump version for Python 3.12 compatibility indication

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name = 'extradict',
     packages = ['extradict'],
-    version = "0.6.0+",
+    version = "0.6.1",
     license = "LGPLv3+",
     author = "João S. O. Bueno",
     author_email = "gwidion@gmail.com",
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
